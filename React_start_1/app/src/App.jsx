94f3e2207f8cf666d08f0d2e2ae944d893ac1694
Alexandrminskiy <alexandrminskiy@gmail.com>
2023-03-22 17:14:53 +0300

    123

---
+++ React_start_1/app/src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react"
+import './App.css'
+
+function App() {
+const obj = {name: 'john', surname: 'smit'};
+
+return (
+
+)
+
+}
+export default App
+https://code.mu/ru/javascript/framework/react/book/prime/basis/variables-values-inserting/
