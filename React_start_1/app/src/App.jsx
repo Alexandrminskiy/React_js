b67f1fc376fe514c569b52e74632b3df6ec8f9d9
Alexandrminskiy <alexandrminskiy@gmail.com>
2023-03-31 17:08:13 +0300

    123

--- React_start_1/app/src/App.jsx
+++ React_start_1/app/src/App.jsx
@@ -1,20 +1,28 @@
 // import { useState } from "react"
 import './App.css'
+// import {nanoid} from 'nanoid'
+import React, { useState } from 'react';
 
-function App() {
-	const arr = ['a', 'b', 'c', 'd', 'e'];
+function App(){
+  const [val1, setVal1] = useState()
+  // const [val2, setVal2] = useState()
+  const [res1, setRes1] = useState()
+  // const [res2, setRes2] = useState()
+  
 
-  return (
-    <>
-    <ul>
-    {arr.map(function(el, i){
-      return <p key={i}><li>{el}</li></p>
-    })}
-    </ul>
-    </>
-  )
+function oN(e){
+  console.log(e.target.value);
+}
+
+  return <>
+ 
+  <input val1={val1} onChange={e => setVal1(e.target.value)}/>
+  <button onClick={()=> setRes1(val1)} onChange={oN}>btn</button>
+  <p>: {res1}</p>
+  </>
 }
 
 export default App
 
 // https://code.mu/ru/javascript/framework/react/book/prime/basis/objects-array-output/
+// https://code.mu/ru/javascript/framework/react/book/prime/states/handling-form-data/
