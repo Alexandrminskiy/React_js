54640f0ca2e734ec3491a4909be26a6b6d702eca
Alexandrminskiy <alexandrminskiy@gmail.com>
2023-05-15 15:48:42 +0300

    какие то уроки

--- React_start_0/app/src/App.jsx
+++ React_start_0/app/src/App.jsx
@@ -3,24 +3,24 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
- const[obj, setObj] = useState({
-  title:"заголовок",
-  time: "20:18"
- });
- const [array, setArray] = useState([1,2,3,4,5]);
+  const [obj, setObj] = useState({
+    title: "заголовок",
+    time: "20:18"
+  });
+  const [array, setArray] = useState([1, 2, 3, 4, 5]);
 
- setObj(prev=>{
-  return {
-    ...prev,
-    title: 'Новый'
-  }
- })
- setArray(prev=>{
-  return {
-    ...prev,
-    [1]:10
-  }
- }) 
+  setObj(prev => {
+    return {
+      ...prev,
+      title: 'Новый'
+    }
+  })
+  setArray(prev => {
+    return {
+      ...prev,
+      [1]: 10
+    }
+  })
 }
 
 export default App;
