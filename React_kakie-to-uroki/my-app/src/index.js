ae27aa9f30048c64221b3ed9beac4a9b171d42b0
Alexandrminskiy <alexandrminskiy@gmail.com>
2023-05-16 17:16:12 +0300

    Урок 6

---
+++ React_kakie-to-uroki/my-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { TEST1, TEST2 } from './test'
+// console.log(TEST1, TEST2);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    123
+  </React.StrictMode>
+);
